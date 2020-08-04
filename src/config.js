c938294323d10631bb6cbc644fd1785731c6a7f9
JZPoteat <jzpoteat@gmail.com>
2020-08-04 01:59:37 -0400

    fixed config file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  // API_ENDPOINT: "https://evening-brushlands-02534.herokuapp.com/api",
-  API_ENDPOINT: "http://localhost:8000/api"
+  API_ENDPOINT: "https://evening-brushlands-02534.herokuapp.com/api",
+  // API_ENDPOINT: "http://localhost:8000/api"
 };
